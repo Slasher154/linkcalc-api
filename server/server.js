75079794b625241f782fdef9a59f548ea0ffafd1
Thanat V <thanatv@thaicom.net>
2018-01-04 09:24:58 +0700

    Add back the require database call code in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const Antenna = require('./classes/antenna');
 const jsonLog = require('./lib/utils').jsonLog;
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
 const path = require('path');
+const moment = require('moment');
 const routes = require('./routes');
+
+
+var mongoose = require('./db/mongoose').mongoose;
 
 var cors = require('cors');
 
@@ -16,6 +22,10 @@ const port = process.env.PORT || 23324;
 app.use(bodyParser.json());
 
 app.use('/', routes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world!');
+// });
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
